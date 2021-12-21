148adf5fa584f2167a74ffe17ded7d6107bfaa5d
SkyEdg <95302660+SkyEdg@users.noreply.github.com>
2021-12-21 03:26:23 +0100

    Create a function that defines the rules for the next iteration of the lottery: Allow to limit the maximum number of entries

--- contracts/Loterie.sol
+++ contracts/Loterie.sol
@@ -24,25 +24,37 @@ contract Loterie is Ownable {
 
     LState public lotteryState = LState.Open;
 
+    uint256 public nb_max_players = 2;
+
     function participate() public payable {
         require(lotteryState == LState.Open);
         require(msg.value >= PARTICIPATION_FEE);
+        require(participantList.length < nb_max_players);
         participantList.push(msg.sender);
+        if (participantList.length == nb_max_players) {
+            lock();
+        }
+        // auto lock the Lotterie when the number of pariticipants is over the nb_max_players value
     }
 
-    function lock() public {
+    function lock() private {
         require(lotteryState == LState.Open);
-        require(participantList.length >= 1);
+        require(participantList.length > 1);
         lotteryState = LState.Locked;
         lockBlock = block.number;
     }
 
-    function withdrawGains() public {
+    // Now the people who withdrawGains for the winner can change the lottery parmeters
+
+    function withdrawGains(uint _new_max_players) public {
         require(lotteryState == LState.Locked);
         require(block.number >= lockBlock + 2);
 
         lotteryState = LState.Open;
-        
+        if (_new_max_players > 1) { // but over one player
+            nb_max_players = _new_max_players; // The winner can define the new max of players
+        }
+
         payable(getWinner()).transfer(address(this).balance);
         delete lockBlock;
         delete participantList;
@@ -61,6 +73,10 @@ contract Loterie is Ownable {
     function getLockBlock() public view returns (uint256) {
         require(lotteryState == LState.Locked);
         return lockBlock;
+    }
+
+    function getLockBlock() public view returns (uint256) {
+        return nb_max_players;
     }
 
     function getParticipantCount() public view returns (uint256) {
