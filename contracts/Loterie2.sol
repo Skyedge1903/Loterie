78b839792a831ba0695800bfc2a19a0c92706f5b
SkyEdg <95302660+SkyEdg@users.noreply.github.com>
2021-12-23 23:43:18 +0100

    Writing and testing the lottery v2
    Deployed and tested on the ropsten network. 100% functional. I have tested many possible cases by hand. Still to be done:
    Unit testing, security audit, gas cost optimization, web3 application.

--- contracts/Loterie2.sol
+++ contracts/Loterie2.sol
@@ -5,17 +5,17 @@ import "./Ownable.sol";
 
 contract Loterie2 is Ownable {
 
-
   struct Lottery {
-    address[] addr;
-    uint[] weight;
+    address[] addrs;
+    uint[] amounts;
 
+    uint total_amount;
     uint max_amount;
+    uint exceeding_tolerance;
     uint lock_block;
 
-    bool was_withdrawn;
+    bool is_dropped;
   }
-
 
   Lottery[] public lotteries;
 
@@ -23,11 +23,13 @@ contract Loterie2 is Ownable {
     uint index
   );
 
-  function create_lottery(uint max_amount, uint lock_block) public {
+  function create_lottery(uint max_amount, uint exceeding_tolerance, uint duration_in_blocks) public {
     Lottery memory l;
+    l.total_amount = 0;
     l.max_amount = max_amount;
-    l.lock_block = lock_block;
-    l.was_withdrawn = false;
+    l.lock_block = block.number + duration_in_blocks;
+    l.exceeding_tolerance = exceeding_tolerance;
+    l.is_dropped = false;
 
     lotteries.push(l);
 
@@ -35,44 +37,111 @@ contract Loterie2 is Ownable {
   }
 
   function participate(uint index) public payable {
-
+    require(is_index_valid(index), "you have entered a wrong index");
+    require(is_lottery_playable(index), "the game is over");
+    require(is_not_exceeding_tolerence(index, msg.value), "you cannot exceed the overrun tolerance");
+    lotteries[index].addrs.push(msg.sender);
+    lotteries[index].amounts.push(msg.value);
+    lotteries[index].total_amount += msg.value;
+    if (!is_lottery_playable(index)) {
+      lotteries[index].lock_block = block.number;
+    }
   }
 
   function withdraw_gains(uint index) public {
-    require(!lotteries[index].was_withdrawn, "lottery was already withdrawn");
+    require(is_index_valid(index), "you have entered a wrong index");
+    require(is_lottery_withdrawable(index), "this lottery is not whithdrawable now");
 
-    lotteries[index].was_withdrawn = true;
+    uint winner_wei = uint(blockhash(lotteries[index].lock_block + 2)) % lotteries[index].total_amount;
+    uint current_wei = 0;
+
+    for (uint i = 0; i < lotteries[index].addrs.length; i++){
+      current_wei += lotteries[index].amounts[i];
+      if (current_wei >= winner_wei) {
+        payable(lotteries[index].addrs[i]).transfer(lotteries[index].total_amount);
+        break;
+      }
+    }
+    lotteries[index].is_dropped = true;
   }
 
-  function is_lottery_locked(uint index) public view {
-
+  function is_index_valid(uint index) public view returns(bool){
+    if (index < 0) {
+      return false;
+    }
+    else if (index >= get_lottery_count()){
+      return false;
+    }
+    else if (lotteries[index].is_dropped){
+      return false;
+    }
+    else {
+      return true;
+    }
   }
 
-  function get_lottery_lock_block(uint index) public view {
-
+  function is_lottery_playable(uint index) public view returns(bool){
+    if (block.number < lotteries[index].lock_block && lotteries[index].total_amount < lotteries[index].max_amount) {
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 
-  function get_lottery_participants(uint index) public view returns(address[] memory, uint[] memory) {
-
+  function is_lottery_withdrawable(uint index) public view returns(bool){
+    if (block.number >= lotteries[index].lock_block + 2) {
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 
-  function get_lottery_max_amount(uint index) public view returns(uint) {
-
+  function is_not_exceeding_tolerence(uint index, uint value) public view returns(bool){
+    uint total_amount_temp = lotteries[index].total_amount + value;
+    uint max_amount_whith_tolerance = lotteries[index].max_amount + lotteries[index].exceeding_tolerance;
+    if (total_amount_temp <= max_amount_whith_tolerance) {
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 
-  function get_lottery_remaining_amount(uint index) public view returns(uint) {
+  function get_winner(uint index) public view returns(address) {
+    require(is_index_valid(index), "you have entered a wrong index");
+    require(is_lottery_withdrawable(index), "this lottery is not whithdrawable now");
+    
+    uint winner_wei = uint(blockhash(lotteries[index].lock_block + 2)) % lotteries[index].total_amount;
+    uint current_wei = 0;
 
+    for (uint i = 0; i < lotteries[index].addrs.length; i++){
+      current_wei += lotteries[index].amounts[i];
+      if (current_wei >= winner_wei) {
+        return lotteries[index].addrs[i];
+      }
+    }
+    return address(this);
   }
 
-  function get_lottery_winner(uint index) public view returns(address) {
-
+  function get_lottery_lock_block(uint index) public view returns(uint) {
+    return lotteries[index].lock_block;
   }
 
-  function was_lottery_withdrawn(uint index) public view returns(bool) {
-    return lotteries[index].was_withdrawn;
+  function get_lottery_participants_number(uint index) public view returns(uint) {
+    return lotteries[index].addrs.length;
   }
 
-  function get_lottery_count(uint index) public view returns(uint) {
+  function get_lottery_total_amount(uint index) public view returns(uint) {
+    return lotteries[index].total_amount;
+  }
+
+  function was_lottery_dropped(uint index) public view returns(bool) {
+    return lotteries[index].is_dropped;
+  }
+
+  function get_lottery_count() public view returns(uint) {
     return lotteries.length;
   }
 
