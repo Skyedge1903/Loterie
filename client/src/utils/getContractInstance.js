262d4679ebd0530cd5a2530bddf94a2fef8c28dc
qdewaghe <q.dewaghe@googlemail.com>
2022-01-07 17:22:07 +0100

    buttons to create a lottery

--- client/src/utils/getContractInstance.js
+++ client/src/utils/getContractInstance.js
@@ -1,7 +1,8 @@
 const getContractInstance = async (web3, contractDefinition) => {
   // get network ID and the deployed address
   const networkId = await web3.eth.net.getId()
-  const deployedAddress = contractDefinition.networks[networkId].address
+  const deployed = contractDefinition.networks[networkId]
+  const deployedAddress = deployed.address
 
   // create the instance
   const instance = new web3.eth.Contract(contractDefinition.abi, deployedAddress)
